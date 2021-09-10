16e3428bcb84fe7a2209f6adf25559eb419a6286
kerem <mollamusaoglu@gmail.com>
2021-09-10 09:16:57 +0300

    disable CallKit Integration
    CallKit integration disabled because ALbaraka Mobil rejected on App Store

--- ios/sdk/src/callkit/JMCallKitProxy.swift
+++ ios/sdk/src/callkit/JMCallKitProxy.swift
@@ -84,7 +84,8 @@ extension CXCallController: CXCallControllerProtocol {
 
     /// Enables the proxy in between CallKit and the consumers of the SDK.
     /// Defaults to enabled, set to false when you don't want to use CallKit.
-    @objc public static var enabled: Bool = true {
+    /// EDIT: set to false because ALbaraka Mobil rejected on App Store
+    @objc public static var enabled: Bool = false {
         didSet {
             if callKitProvider == nil {
                 provider.invalidate()
