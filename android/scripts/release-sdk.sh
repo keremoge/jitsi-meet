24dd3edfb62122bc65d5baaa79db94335974f5ee
Kerem ÖĞE <kerem.oge@albarakatech.com>
2024-11-19 12:47:28 +0300

    Jitsi Meet SDK + dependencies: 10.2.1

--- android/scripts/release-sdk.sh
+++ android/scripts/release-sdk.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e -u
-
+set -e -u -o pipefail
 
 THIS_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
 DEFAULT_MVN_REPO="${THIS_DIR}/../../../jitsi-maven-repository/releases"
@@ -9,7 +8,6 @@ THE_MVN_REPO=${MVN_REPO:-${1:-$DEFAULT_MVN_REPO}}
 MVN_HTTP=0
 DEFAULT_SDK_VERSION=$(grep sdkVersion ${THIS_DIR}/../gradle.properties | cut -d"=" -f2)
 SDK_VERSION=${OVERRIDE_SDK_VERSION:-${DEFAULT_SDK_VERSION}}
-JSC_VERSION="r"$(jq -r '.dependencies."jsc-android"' ${THIS_DIR}/../../node_modules/react-native/package.json | cut -d . -f 1 | cut -c 2-)
 
 if [[ $THE_MVN_REPO == http* ]]; then
     MVN_HTTP=1
@@ -23,39 +21,10 @@ export MVN_REPO=$THE_MVN_REPO
 echo "Releasing Jitsi Meet SDK ${SDK_VERSION}"
 echo "Using ${MVN_REPO} as the Maven repo"
 
- if [[ $MVN_HTTP == 1 ]]; then
-    # Push JSC
-    echo "Pushing JSC ${JSC_VERSION} to the Maven repo"
-    pushd ${THIS_DIR}/../../node_modules/jsc-android/dist/org/webkit/android-jsc/${JSC_VERSION}
-    mvn \
-        deploy:deploy-file \
-        -Durl=${MVN_REPO} \
-        -DrepositoryId=${MVN_REPO_ID} \
-        -Dfile=android-jsc-${JSC_VERSION}.aar \
-        -Dpackaging=aar \
-        -DgeneratePom=false \
-        -DpomFile=android-jsc-${JSC_VERSION}.pom || true
-    popd
-else
-    # Push JSC, if necessary
-    if [[ ! -d ${MVN_REPO}/org/webkit/android-jsc/${JSC_VERSION} ]]; then
-        echo "Pushing JSC ${JSC_VERSION} to the Maven repo"
-        pushd ${THIS_DIR}/../../node_modules/jsc-android/dist/org/webkit/android-jsc/${JSC_VERSION}
-        mvn \
-            deploy:deploy-file \
-            -Durl=${MVN_REPO} \
-            -Dfile=android-jsc-${JSC_VERSION}.aar \
-            -Dpackaging=aar \
-            -DgeneratePom=false \
-            -DpomFile=android-jsc-${JSC_VERSION}.pom
-        popd
-    fi
-
-    # Check if an SDK with that same version has already been released
-    if [[ -d ${MVN_REPO}/org/jitsi/react/jitsi-meet-sdk/${SDK_VERSION} ]]; then
-        echo "There is already a release with that version in the Maven repo!"
-        exit 1
-    fi
+# Check if an SDK with that same version has already been released
+if [[ $MVN_HTTP == 0 && -d ${MVN_REPO_PATH}/org/jitsi/react/jitsi-meet-sdk/${SDK_VERSION} ]]; then
+    echo "There is already a release with that version in the Maven repo!"
+    exit 1
 fi
 
 # Now build and publish the Jitsi Meet SDK and its dependencies
@@ -76,3 +45,4 @@ fi
 
 # Done!
 echo "Finished! Don't forget to push the tag and the Maven repo artifacts."
+
